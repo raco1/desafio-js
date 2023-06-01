9f92e40ec431de534cdddcd02c201ad7d9c2f6af
raco1 <113068055+raco1@users.noreply.github.com>
2023-06-01 11:26:58 -0300

    Add files via upload

---
+++ desafio js/main.js
@@ -0,0 +1,77 @@
+//  Deixei tudo comentado, caso queira testar uma atividade só retirar o comentário da mesma.
+ 1. 
+// alert(`Hello World!`);
+
+ 2.
+// alert(`Vamos realizar a soma de dois números!`);
+
+// let numberOne = Number(prompt(`Digite o primeiro número:`));
+// let numberTwo = Number(prompt(`Digite o segundo número:`));
+
+// const sum = numberOne + numberTwo;
+
+// alert(`A soma de ${numberOne} e ${numberTwo} é de ${sum}!!`);
+
+3. 
+// let numberX = 27
+// if (typeof numberX == 'number'){
+//     alert(`É um número`)    
+// }else{
+//     alert(`Não é um numero`)
+// }
+
+4.
+// let numberY = `Rafael`
+// if (typeof numberY == 'string'){
+//     alert(`É uma string`)    
+// }else{
+//     alert(`Não é uma string`)
+// }
+
+5.
+// let bool = true
+// if (typeof bool == 'boolean'){
+//     alert(`É um boolean`)    
+// }else{
+//     alert(`Não é um boolean`)
+// }
+
+6.
+// let firstNumber = 10;
+// let secondNumber = 5;
+
+// const sub = firstNumber - secondNumber;
+
+// alert(`A subtração dos números é ${sub}`);
+
+7.
+// let firstNumber = 10;
+// let secondNumber = 5;
+
+// const multi = firstNumber * secondNumber;
+
+// alert(`A multipliacação dos números é ${multi}`);
+
+8.
+// let firstNumber = 10;
+// let secondNumber = 5;
+
+// const div = firstNumber / secondNumber;
+
+// alert(`A divisão dos números é ${div}`);
+
+9.
+// let numberEven = 4;
+// if (numberEven % 2 == 0){
+//     alert(`O número é par`);
+// } else{
+//     alert (`Não é um número par`)
+// }
+
+10.
+// let numberEven = 3;
+// if (numberEven % 2 !== 0){
+//     alert(`O número é ímpar`);
+// } else{
+//     alert (`Não é um número ímpar`)
+// }
